65e71e6c3ba6a9ef0ec125bdc17ced1e20c6c5e0
Marcio Samogin <marcio@unicamp.br>
2025-12-05 16:34:21 -0300

    Update config-ubuntu.sh

--- config-ubuntu.sh
+++ config-ubuntu.sh
@@ -11,11 +11,13 @@ git clone https://github.com/marsamogin/linux
 echo "Copiar arquivos prontos de config. para destino"
 mv -f /tmp/linux/chrony.conf /etc
 mv -f /tmp/linux/bacula-fd.conf /etc/bacula
-mv -f /tmp/linux/main.cf /etc/postfix
 mv -f /tmp/linux/*.sh /root/shell
+mv -f /root/shell/main.cf-ubuntu /etc/postfix/main.cf
 chmod +x /root/shell/*.sh
 echo "Definir nome do host no arquivo do Bacula"
 sed -i "s/trocar/$(hostname)/g" /etc/bacula/bacula-fd.conf
+echo "Definir o nome do host no arquivo do Postfix"
+sed -i "s/trocar/$(hostname)/g" /etc/postfix/main.cf
 echo "Habilitar os servicos postfix e bacula da maquina"
 systemctl enable postfix && systemctl enable bacula-fd
 echo "Ajustar a configuracao do sshd_config"
