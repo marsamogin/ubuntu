169fb9023c5009d7c2d8a31b40e9c8924250e374
Marcio Samogin <marcio@unicamp.br>
2026-03-25 18:29:53 -0300

    Update create-users-ubuntu.sh

--- create-users-ubuntu.sh
+++ create-users-ubuntu.sh
@@ -2,23 +2,23 @@
 ##########################################
 ##########################################
 #echo "Configurar acesso usuario ludgero"
-#/root/shell/./create-user-ubuntu-ludgero.sh
+#./create-user-ubuntu-ludgero.sh
 ##########################################
 echo "Configurar acesso usuario marcio"
-/root/shell/./create-user-ubuntu-marcio.sh
+./create-user-ubuntu-marcio.sh
 ##########################################
 echo "Configurar acesso usuario keite"
-/root/shell/./create-user-ubuntu-keite.sh
+./create-user-ubuntu-keite.sh
 ##########################################
 #echo "Configurar acesso usuario otoniel"
-#/root/shell/./create-user-ubuntu-otoniel.sh
+#./create-user-ubuntu-otoniel.sh
 ##########################################
 echo "Configurar acesso usuario danfeijo"
-/root/shell/./create-user-ubuntu-danfeijo.sh
+./create-user-ubuntu-danfeijo.sh
 ##########################################
 # Capturar o nome da maquina
 hostname=$(hostname)
 echo "Copiar as chaves privadas para a maquina Linux auxiliar"
 scp *@$hostname marcio@143.106.108.5:/tmp
 rm -rf *@$hostname
-/root/shell/./config-ubuntu.sh
+./config-ubuntu.sh
