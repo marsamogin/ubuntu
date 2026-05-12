f1059114f02c9a2c4a6b2a6379b45898f15bb0d4
Marcio Samogin <marcio@unicamp.br>
2026-05-12 18:24:24 -0300

    Update config-ubuntu-inicial.sh

--- config-ubuntu-inicial.sh
+++ config-ubuntu-inicial.sh
@@ -2,5 +2,6 @@ s #!/bin/bash -xv
 # Redefine o mapa do teclado para ABNT
 localectl set-locale LANG=pt_BR.UTF-8
 dpkg-reconfigure keyboard-configuration
-# Adicionar permissao de execuçao aos aqruivos
+# Adicionar permissao de execuçao aos demais arquivos
 chmod +x *.sh
+./create-users-ubuntu.sh
